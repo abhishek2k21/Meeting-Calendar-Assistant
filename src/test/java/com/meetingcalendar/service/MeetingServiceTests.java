5f791f3014dadfadb2586d9ad33d93e7af99ec14
Abhishek Kumar <123145812+abhishek2k21@users.noreply.github.com>
2024-10-22 08:07:27 +0530

    Update MeetingServiceTests.java

--- src/test/java/com/meetingcalendar/service/MeetingServiceTests.java
+++ src/test/java/com/meetingcalendar/service/MeetingServiceTests.java
@@ -38,7 +38,7 @@ public class MeetingServiceTests {
     @Test
     public void testBookMeeting_EmployeeNotFound() throws ParseException {
         Meeting meeting = new Meeting();
-        meeting.setEmployee(1L); // Change to Long
+        meeting.setEmployee(1L); 
         meeting.setDate("15/10/2024");
         meeting.setTime("10:00");
 
@@ -50,19 +50,18 @@ public class MeetingServiceTests {
 
     @Test
     public void testBookMeeting_Conflict() throws ParseException {
-        // Setup employee with existing meetings
         Employee employee = new Employee();
         employee.setEmpID(1L);
         employee.setMeetings(List.of("15/10/2024 10:00"));
 
         Meeting meeting = new Meeting();
-        meeting.setEmployee(1L); // Change to Long
+        meeting.setEmployee(1L); 
         meeting.setDate("15/10/2024");
         meeting.setTime("10:00");
 
         when(employeeRepository.findById(1L)).thenReturn(Optional.of(employee));
 
         int result = meetingService.bookMeeting(meeting);
-        assertEquals(2, result); // Conflict expected
+        assertEquals(2, result); 
     }
 }
